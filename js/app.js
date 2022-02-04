be844729520fc38f6212855eced7ab0e4a983f21
tyebean <flower.webster@gmail.com>
2022-02-04 13:01:04 -0500

    outlined renders if

--- js/app.js
+++ js/app.js
@@ -16,42 +16,51 @@ let boardArr, turn, winner
 /*------------------------ Cached Element References ------------------------*/
 
 const squares = document.querySelectorAll('.square')
-
+const message = document.querySelector('#message') 
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 
 
 /*-------------------------------- Functions --------------------------------*/
-// init()
+init()
 function init(){
-  console.log('init function')
-  //should initilize the state variables
-  //init board array to 9 nulls to rep empty squares
-  // squareArr = [null, null, null, null, null, null, null, null, null]
-  //   //9 elements will 'map' to each square
-  // let mapSquare = squareArr.map(function(a){
-  //   //use a for loop to map each null to each indv square using the index (0 = top left quare and so on)
-  // })
-    //index 0 reps top left square and 1 reps top mid square
-
-  //init whos turn it is
-  //init winner var to null (no winner yet)
-  //render() these state variables with an outside render function
+  console.log('init function SHELL works') 
+  boardArr = [null, null, null, null, null, null, null, null, null] 
+  console.log(boardArr)
+  let playerX = 1
+  let playerO = -1
+  console.log(playerX, playerO)
+  turn = 1
+  winner = null 
+  //render()
 }
+
+
+
+
+
+
 
 
 function render(){
-  //loop over board array 
-}
-  boardArr = [null, null, null, null, null, null, null, null, null]
-  console.log(boardArr)
-    //9 elements will 'map' to each square
-  let mapSquares = boardArr.map(function(a){
-    //use a for loop to map each null to each indv square using the index (0 = top left quare and so on) 
-    for(let i = 0; i boardArr.length; i++){
-      console.log("hey")
-    }
-  })
-console.log(mapSquare)
-
+  for (let i = 0; i < boardArr.length; i++){
+    squares[i].style.backgroundColor = 'red'
+    //later note: style each square dependant on the value contained in the current cell being iterated over (-1, 1, or null.)
+  }
+  if (winner === 1) {
+    // later note: render these msgs 
+    //render playerX's turn
+    console.log('plr 1')
+  } else if (winner === -1) {
+    //render plrO's turn
+    console.log('plr -1')
+  } else if (winner === 'T') {
+    //render its a tie
+    console.log("it's a Tie")
+  } else {
+    //render congrats!
+    console.log("congrats! this player won!")
+  }
+} 
+render() 
