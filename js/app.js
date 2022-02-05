6f109117eff9b38953dab1cf30399aa06624e68c
tyebean <flower.webster@gmail.com>
2022-02-05 15:08:59 -0500

    work on #3

--- js/app.js
+++ js/app.js
@@ -1,14 +1,26 @@
 /*-------------------------------- Constants --------------------------------*/
-//constants contain 8 arrays of the winning possibilities
 
-
-
+const wins = [
+  [sq0, sq1, sq2],
+  [sq0, sq4, sq8],
+  [sq0, sq3, sq6],
+  [sq1, sq4, sq7],
+  [sq2, sq4, sq6],
+  [sq2, sq5, sq8],
+  [sq3, sq4, sq5],
+  [sq6, sq7, sq8]
+]
+// console.log(wins)
+// console.log(wins[1])
+//if these winning vals reduce to a val of 3 then someone has won
 
 /*---------------------------- Variables (state) ----------------------------*/
 
 let boardArr, turn, winner 
-//variable winner reps
-//a player that won
+//boardArr - reps squares on board (array)
+//turn - tracks whos turn it is
+//winner - 
+//variable winner rep
 //a tie has occured
 //game still in play
 
@@ -23,31 +35,35 @@ const message = document.querySelector('#message')
 
 
 /*-------------------------------- Functions --------------------------------*/
+
+
 init()
 function init(){
-  console.log('init function SHELL works') 
-  boardArr = [null, null, null, null, null, null, null, null, null] 
+  boardArr = [null, null, null, null, null, null, null, null, null]
+  //The null values will refer to a square being empty, 1 will translate to an X, and -1 will translate to an O
   console.log(boardArr)
-  let playerX = 1
-  let playerO = -1
-  console.log(playerX, playerO)
   turn = 1
   winner = null 
   //render()
 }
 
-
-
-
-
-
-
+boardArr.forEach((square, idx) => {
+  idx = squares[0].length
+  square = 1
+  if(square === 1){
+    //put an x on that square? 
+  } else if (winner === -1){
+    //
+  }
+  
+  // console.log(idx)
+})
 
 function render(){
-  for (let i = 0; i < boardArr.length; i++){
-    squares[i].style.backgroundColor = 'red'
-    //later note: style each square dependant on the value contained in the current cell being iterated over (-1, 1, or null.)
-  }
+  // for (let i = 0; i < boardArr.length; i++){
+  //   squares[i].style.backgroundColor = 'red'
+  //   //later note: style each square dependant on the value contained in the current cell being iterated over (-1, 1, or null.)
+  // } 
   if (winner === 1) {
     // later note: render these msgs 
     //render playerX's turn
@@ -64,3 +80,16 @@ function render(){
   }
 } 
 render() 
+
+
+//handle click fucntion #5
+// obtain the index of the square that is being clicked on (use squares[1]) to get the index of the amount of swuares we have?
+
+// function handleClick(){
+//   console.log(squares[1])
+// }
+// console.log(handleClick)
+
+
+//the secret is that when you gather up the .square class in the JS file, it creates an array-like object
+//**which you can then correspond with the index of your board array
